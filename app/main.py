04f8b76ee102219772a0381be1e77e45d8e425a2
JCarbajal22 <u202121881@upc.edu.pe>
2025-12-06 10:53:45 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import locals, local_categories, comments
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Locals Service")
+
+origins = [
+    "http://localhost:5173",                # para desarrollo local
+    "https://alquilaezz.netlify.app",       # tu front en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # o ["*"] si quieres permitir todos (no recomendado para prod)
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],            # "Content-Type", "Authorization", etc.
+)
 
 app.include_router(locals.router)
 app.include_router(local_categories.router)
